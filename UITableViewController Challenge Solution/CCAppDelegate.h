1cb1f532325f1a7da30372ee85c2e7d53b643f66
x <x@xs-Mac.local>
2014-05-10 23:28:32 -0400

    Initial Commit

---
+++ UITableViewController Challenge Solution/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by x on 5/10/14.
+//  Copyright (c) 2014 UU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewController Challenge Solution
+//
+//  Created by x on 5/10/14.
+//  Copyright (c) 2014 UU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCAppDelegate class]));
+    }
+}
